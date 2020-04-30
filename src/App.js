963ee740ed4ebd8091c9d59aba955b77a9eaced9
ignaciozanotti <ignacio.zanotti@gmail.com>
2020-04-30 16:50:33 -0300

    form onChange & submits

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Contador from './components/Contador';
 import Lista from './components/Lista';
+import Formulario from './components/Formulario';
 
 
 function App() {
   return (
     <div>
       <h1>Hola Hooks!</h1>
-      <Contador />
-      <Lista />
+        <div className="container mt-5">
+          <Contador />
+          <Lista />
+          <Formulario />
+        </div>
     </div>
   );
  }
